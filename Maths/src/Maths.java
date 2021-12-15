b3bf2ac880e327eb408d66eaeae4e2b0d9e4236f
pendli-sheshank <sheshank3336@gmail.com>
2021-12-15 17:53:01 +0530

    area of triangle

--- Maths/src/Maths.java
+++ Maths/src/Maths.java
@@ -1,6 +1,7 @@
 import java.util.*;
 public class Maths {
 	Scanner input = new Scanner(System.in);
+	
 	public  void areaofCircle() {
 		System.out.println("Enter the radius");
 		int radius = input.nextInt();
@@ -18,10 +19,20 @@ public class Maths {
 		System.out.println(area);
 	}
 	
+	public void areaofTriangle() {
+		System.out.println("Enter the height");
+		int height = input.nextInt();
+		System.out.println("Enter base");
+		int base = input.nextInt();
+		int area = (height*base)/2;
+		System.out.println(area);
+	}
+	
 	public static void main(String args []) {
 		Maths ac = new Maths();
 		ac.areaofCircle();
 		ac.areaOfRectangle();
+		ac.areaofTriangle();
 	}
 
 }
