2cd1e1bca74e0ca4f4c4909f73820e6f9e0a69a2
pendli-sheshank <sheshank3336@gmail.com>
2021-12-15 17:16:17 +0530

    start

---
+++ Maths/src/Maths.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Maths {
+	Scanner input = new Scanner(System.in);
+	public  void areaofCircle() {
+		System.out.println("Enter the radius");
+		int radius = input.nextInt();
+		double area = (3.14)*radius*radius;
+		System.out.println(area);
+		
+	}
+	
+	public void areaOfRectangle() {
+		System.out.println("Enter the length");
+		int length = input.nextInt();
+		System.out.println("Enter the bredth");
+		int bredth = input.nextInt();
+		int area = length*bredth;
+		System.out.println(area);
+	}
+	
+	public static void main(String args []) {
+		Maths ac = new Maths();
+		ac.areaofCircle();
+		ac.areaOfRectangle();
+	}
+
+}
